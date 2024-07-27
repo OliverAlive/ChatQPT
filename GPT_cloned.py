039e8a9b98820714811345f31563691a802a4126
OliverAlive <153698099+OliverAlive@users.noreply.github.com>
2024-07-27 23:22:49 +0800

    Add files via upload

--- GPT_cloned.py
+++ GPT_cloned.py
@@ -3,7 +3,7 @@ from langchain_openai import ChatOpenAI
 import os
 from langchain.memory import ConversationBufferMemory
 def get_chat_response(prompt,memory,openai_api_key):
-    model = ChatOpenAI(model = "gpt-4o-mini",openai_api_key = openai_api_key,openai_api_base="https://api.aigc369.com/v1")
+    model = ChatOpenAI(model = "gpt-3.5-turbo",openai_api_key = openai_api_key,openai_api_base="https://api.aigc369.com/v1")
     chain = ConversationChain(llm=model,memory=memory)
     response = chain.invoke({"input":prompt})
     return response["response"]
